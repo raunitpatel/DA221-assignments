adb227d242db0c55b0ca125cbd5eb8ec3325d243
raunitpatel1995 <patelraunit1995@gmail.com>
2024-02-12 10:37:39 +0530

    added assgn1 & assgn2

---
+++ assignment-1/heuristic_manhattan.cpp
@@ -0,0 +1,294 @@
+#include<bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+//node class
+struct node
+{
+    vector<int> orientation;
+    int g_cost, h_cost, f_cost;
+    node* parent;
+
+    node()
+    {
+        g_cost = 0;
+        h_cost = 0;
+        f_cost = 0;
+        parent = NULL;
+    }
+};
+
+//operator overloading for priority_queue
+struct comp
+{
+    bool operator()(const node* lhs, const node* rhs) const
+    {
+        return lhs->f_cost > rhs->f_cost;
+    }
+};
+
+int no_of_elements=9;
+vector<int> goal_test;
+vector<node*> already_explored;
+priority_queue<node*, vector<node*>, comp> to_be_explored;
+
+//print board
+void print_board(node* n)
+{
+    int dim = sqrt(no_of_elements);
+    int k = 0;
+    for(int i = 0; i < dim; ++i)
+    {
+        for(int j = 0; j < dim; ++j)
+            cout << n->orientation[k++] << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+//print solution
+void reconstruct_path_from_root_to_goal(node* n)
+{
+    node* temp = n;
+    vector<node*> totalpath;	//it's a list for containing all the nodes in the path from the root to the goal
+    while(temp != NULL)
+    {
+        totalpath.push_back(temp);
+        temp = temp->parent;
+    }
+
+    int sz = totalpath.size();
+    //print number of moves required
+    cout << "Moves Required: " << sz-1 << endl;
+
+    //show the moves one by one
+    for(int i = sz-1; i >= 0; i--){
+        cout<<"f = "<<totalpath[i]->f_cost<<" g = "<<totalpath[i]->g_cost<<" h = "<<totalpath[i]->h_cost<<endl;
+        print_board(totalpath[i]);
+    }
+}
+
+//calculate heuristic
+int calculate_manhattan_distance(node* n)
+{
+    int distance = 0;
+    int dim = sqrt(no_of_elements);
+    
+    for(int i = 0; i < no_of_elements; ++i)
+    {
+        if(n->orientation[i] != 0) // Skip the empty tile
+        {
+            int current_row = i / dim;
+            int current_col = i % dim;
+            int goal_row = goal_test[n->orientation[i]] / dim;
+            int goal_col = goal_test[n->orientation[i]] % dim;
+            
+            distance += abs(current_row - goal_row) + abs(current_col - goal_col);
+        }
+    }
+    
+    return distance;
+}
+
+//build successor node
+node* create_successor_node(node* state, int pos1, int pos2)
+{
+    node* new_state = new node();
+    new_state->orientation = state->orientation;
+    swap(new_state->orientation[pos1], new_state->orientation[pos2]);
+    new_state->h_cost = calculate_manhattan_distance(new_state);
+    new_state->g_cost = state->g_cost+1;
+    new_state->f_cost = new_state->h_cost + new_state->g_cost;
+    new_state->parent = state;
+
+    return new_state;
+}
+
+//generate successors
+vector<node*> get_successor(node* n)
+{
+    int pos, row, col, dim;
+    for(int i = 0; i < no_of_elements; ++i)
+    {
+        if(n->orientation[i] == 0)
+        {
+            pos = i;
+            break;
+        }
+    }
+    dim = sqrt(no_of_elements);
+    row = pos / dim;
+    col = pos % dim;
+
+    vector<node*> successors;
+
+    if(col != 0)
+        successors.push_back(create_successor_node(n, pos, pos-1));
+
+    if(col != dim-1)
+        successors.push_back(create_successor_node(n, pos, pos+1));
+
+    if(row != 0)
+        successors.push_back(create_successor_node(n, pos, pos-dim));
+
+    if(row != dim-1)
+        successors.push_back(create_successor_node(n, pos, pos+dim));
+
+    return successors;
+}
+
+//return true if goal
+bool is_Goal(node* n)
+{
+    return (n->h_cost == 0) ? 1 : 0;
+}
+
+bool check_already_explored_list(node* n)
+{
+    int sz = already_explored.size(), j;
+    for(int i = 0; i < sz; ++i)
+    {
+        for(j = 0; j < no_of_elements; ++j)
+        {
+            if(n->orientation[j] != already_explored[i]->orientation[j])
+                break;
+        }
+        if(j == no_of_elements)
+            return 1;
+    }
+    return 0;
+}
+
+void A_star_search(node* n)
+{
+    n->h_cost = calculate_manhattan_distance(n);
+    n->f_cost = n->h_cost;
+    n->parent = NULL;
+    to_be_explored.push(n);
+
+    bool flag;
+    int tentative_g_cost, sz, k;
+    node *current, *temp;
+    vector<node*> current_successors;
+    priority_queue<node*, vector<node*>, comp> pq;
+
+    while(!to_be_explored.empty())
+    {
+        current = to_be_explored.top();
+        to_be_explored.pop();
+        already_explored.push_back(current);
+
+        if(is_Goal(current))
+        {
+            reconstruct_path_from_root_to_goal(current);
+            return;
+        }
+
+        current_successors.clear();
+        current_successors = get_successor(current);
+
+        sz = current_successors.size();
+        for(int i = 0; i < sz; ++i)
+        {
+            if(check_already_explored_list(current_successors[i]))
+                continue;
+            tentative_g_cost = current->g_cost+1;
+
+            while(!pq.empty())
+                pq.pop();
+            while(!to_be_explored.empty())
+            {
+                temp = to_be_explored.top();
+                to_be_explored.pop();
+
+                flag = 0;
+                for(k = 0; k < no_of_elements; ++k)
+                    if(current_successors[i]->orientation[k] != temp->orientation[k])
+                        break;
+                if(k == no_of_elements)
+                    flag = 1;
+
+
+                if(flag && tentative_g_cost < temp->g_cost)
+                {
+                    temp->parent = current;
+                    temp->g_cost = tentative_g_cost;
+                    temp->f_cost = temp->g_cost + temp->h_cost;
+                }
+                pq.push(temp);
+            }
+            if(!flag)
+                pq.push(current_successors[i]);
+            to_be_explored = pq;
+        }
+    }
+
+    return;
+}
+
+int main()
+{
+ 
+    int testcase=0;
+    unsigned seed = 1;
+    vector<int> arr(9);
+    vector<node *> start_list(10);
+    for(int i=0;i<9;i++){
+        arr[i]=i;
+        goal_test.push_back(i);
+    }
+    while(testcase<10){
+            
+        shuffle(arr.begin(),arr.end(),default_random_engine(seed));
+        
+        vector<int> start(9);
+        start=arr;
+        
+        int count1=0;
+        for(int i=0;i<8;i++){
+            for(int j=i+1;j<9;j++){
+                if(start[j] && start[i] && start[j]<start[i]){
+                    count1++;
+                }
+            }
+        }
+        
+        
+        seed=seed+1;
+        if(count1%2==0){
+            start_list[testcase] = new node();
+            for(int i=0;i<9;i++){
+                start_list[testcase]->orientation.push_back(start[i]);
+            }
+            
+            for(auto x:start){
+                cout<<x<<" ";
+            }
+            
+            cout<<endl;
+            cout<<count1<<endl;
+            testcase++;
+        }
+    }
+    cout<<"------------------------------"<<endl;
+    auto start = high_resolution_clock::now();
+    A_star_search(start_list[0]);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken to reach Goal state: "<< duration.count() / 1000<< " milliseconds" << endl;
+    cout<<"Nodes removed from the frontier (): "<<already_explored.size()<<endl;
+    cout<<"------------------------------"<<endl;
+    start = high_resolution_clock::now();
+
+    already_explored.clear();
+    priority_queue<node*, vector<node*>, comp> empty;
+    swap(to_be_explored, empty);
+
+    A_star_search(start_list[1]);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken to reach Goal state: "<< duration.count() / 1000<< " milliseconds" << endl;
+    cout<<"Nodes removed from the frontier (): "<<already_explored.size()<<endl;
+
+
+}
+
---
+++ assignment-2/tic-tac-toe-with-minimax-algo.cpp
@@ -0,0 +1,224 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define SIZE 3
+#define COMPUTER 1
+#define HUMAN 2
+#define COMPUTERMOVE 'O'
+#define HUMANMOVE 'X'
+
+void show_instruct(){
+    cout<<"\n Choose a cell numbered from 1 to 9\n";
+    cout<<"\t\t\t 1 | 2 | 3 \n";
+    cout<<"\t\t\t 4 | 5 | 6 \n";
+    cout<<"\t\t\t 7 | 8 | 9 \n";
+}
+void show_board(char board[][SIZE]){
+    cout<<"\t\t\t "<<board[0][0]<<" | "<<board[0][1]<<" | "<<board[0][2]<<endl;
+    cout<<"\t\t\t "<<board[1][0]<<" | "<<board[1][1]<<" | "<<board[1][2]<<endl;
+    cout<<"\t\t\t "<<board[2][0]<<" | "<<board[2][1]<<" | "<<board[2][2]<<endl;
+
+}
+void init_board(char board[][SIZE]){
+    for(int i=0;i<SIZE;i++){
+        for(int j=0;j<SIZE;j++){
+            board[i][j]='*';
+        }
+    }
+}
+void declare_winner(int whoseturn){
+    if(whoseturn==COMPUTER) cout<<"COMPUTER WON THE GAME\n";
+    else cout<<"HUMAN WON THE GAME\n";
+}
+bool row_crossed(char board[][SIZE]){
+    for(int i=0;i<SIZE;i++){
+        if(board[i][0]==board[i][1] && board[i][1]==board[i][2] && board[i][0]!='*' )return true;
+    }
+    return false;
+}
+bool column_crossed(char board[][SIZE]){
+    for(int i=0;i<SIZE;i++){
+        if(board[0][i]==board[1][i] && board[1][i]==board[2][i] && board[1][i]!='*' )return true;
+    }
+    return false;
+}
+bool diagonal_crossed(char board[][SIZE]){
+    if(board[0][0]==board[1][1] && board[1][1]==board[2][2] && board[0][0]!='*' )return true;
+    if(board[0][2]==board[1][1] && board[1][1]==board[2][0] && board[0][2]!='*' )return true;
+    return false;
+}
+bool game_over(char board[][SIZE]){
+    return (row_crossed(board) || column_crossed(board) || diagonal_crossed(board));
+}
+int openLinesForPlayer(char board[][SIZE],char player){
+    int openLines=0;
+    char opponent =(player==COMPUTERMOVE)?HUMANMOVE:COMPUTERMOVE;
+    for(int i=0;i<SIZE;i++){
+        bool rowOpen =true,colOpen=true;
+        for(int j=0;j<SIZE;j++){
+            if(board[i][j]==opponent)rowOpen=false;
+            if(board[j][i]==opponent)colOpen=false;
+        }
+        openLines+=rowOpen+colOpen;
+    }
+    bool diag1Open =true,diag2Open=true;
+    for(int i=0;i<SIZE;i++){
+        if(board[i][i]==opponent)diag1Open=false;
+        if(board[i][SIZE-i-1]==opponent)diag2Open=false;
+    }
+    openLines+=diag1Open+diag2Open;
+    return openLines;
+}
+bool check_winner(char board[][SIZE], char player) {
+    for (int i = 0; i < SIZE; ++i) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+            return true;
+        }
+    }
+    for (int j = 0; j < SIZE; ++j) {
+        if (board[0][j] == player && board[1][j] == player && board[2][j] == player) {
+            return true;
+        }
+    }
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return true;
+    }
+    return false;
+}
+int evaluate_fn(char board[][SIZE]){
+    int open_for_AI=openLinesForPlayer(board,COMPUTERMOVE);
+    int open_for_human=openLinesForPlayer(board,HUMANMOVE);
+    if(check_winner(board,COMPUTER)) return INT_MAX;
+    else if(check_winner(board,HUMAN)) return INT_MIN;
+    return open_for_AI-open_for_human;
+}
+
+int minimax(char board[][SIZE],int depth,bool isAI){
+    int score=0,bestScore=0;
+    if(game_over(board)==true || depth>=9){
+        return evaluate_fn(board);
+    }
+    else{
+        if(isAI){
+            bestScore=INT_MIN;
+            for(int i=0;i<SIZE;i++){
+                for(int j=0;j<SIZE;j++){
+                    if(board[i][j]=='*'){
+                        board[i][j]=COMPUTERMOVE;
+                        score=minimax(board,depth+1,false);
+                        board[i][j]='*'; 
+                        if(score>bestScore) bestScore=score;
+                    }
+                }
+            }
+            return bestScore;
+        }
+        else {
+            bestScore=INT_MAX;
+            for(int i=0;i<SIZE;i++){
+                for(int j=0;j<SIZE;j++){
+                    if(board[i][j]=='*'){
+                        board[i][j]=HUMANMOVE;
+                        score=minimax(board,depth+1,true);
+                        board[i][j]='*';
+                        if(score<bestScore) bestScore=score;
+                    }
+                }
+            }
+            return bestScore;
+        }
+    }
+}
+int bestmove(char board[][SIZE], int moveIndex){
+    int x=-1,y=-1;
+    int score =0,bestscore=INT_MIN;
+    for(int i=0;i<SIZE;i++){
+        for(int j=0;j<SIZE;j++){
+            if(board[i][j]=='*'){
+                board[i][j]=COMPUTERMOVE;
+                score=minimax(board,moveIndex+1,false);
+                board[i][j]='*';
+                if(score>bestscore){
+                    bestscore=score;
+                    x=i;
+                    y=j;
+                }
+            }
+        }
+    }
+    return x*3+y;
+}
+void play_tto(int whoseturn){
+    char board[SIZE][SIZE];
+    int moveIndex =0,x=0,y=0;
+    init_board(board);
+    show_instruct();
+
+    while(game_over(board)==false && moveIndex!=SIZE*SIZE){
+        int n;
+        if(whoseturn == COMPUTER){
+            n =bestmove(board,moveIndex);
+            x=n/SIZE;
+            y=n%SIZE;
+            board[x][y] =COMPUTERMOVE;
+            cout<<"Computer has put a "<<COMPUTERMOVE<<" in cell "<<n+1<<endl;
+            show_board(board);
+            moveIndex++;
+            whoseturn=HUMAN;
+        }
+        else if(whoseturn == HUMAN){
+            cout<<"You can insert in following position : ";
+            for(int i=0;i<SIZE;i++){
+                for(int j=0;j<SIZE;j++){
+                    if(board[i][j]=='*') cout<<i*3+j+1<<" ";
+                }
+            }
+            cout<<"\n\n Enter the position = ";
+            cin>>n;
+            n--;
+            x=n/SIZE;
+            y=n%SIZE;
+            if(board[x][y] == '*' && n<9 && n>=0){
+                board[x][y]=HUMANMOVE;
+                cout<<"Human has put a "<<HUMANMOVE<<" in cell "<<n+1<<endl;
+                show_board(board);
+                moveIndex++;
+                whoseturn=COMPUTER;
+            }
+            else if(board[x][y] != '*' && n<9 && n>=0){
+                cout<<"\nPosition is Occupied, please choose another position\n";
+            }
+            else{
+                cout<<"\nInvalid Position\n";
+            }
+        }
+    }
+    if(game_over(board)==false && moveIndex==SIZE*SIZE){
+        cout<<"GAME IS A DRAW\n\n";
+    }
+    else{
+        if(whoseturn==COMPUTER)whoseturn=HUMAN;
+        else whoseturn=COMPUTER;
+        declare_winner(whoseturn);
+    }
+    cout<<"\n---------------------------------\n";
+    cout<<"\n---------------------------------\n\n";
+
+}
+
+int main(){
+    cout<<"\n---------------------------------\n\n";
+    cout<<"\t TIC-TAC-TOE\n";
+    cout<<"\n---------------------------------\n\n";
+    char cont='y';
+    do{
+        char choice;
+        cout<<"Do you want to start first(y/n) : ";
+        cin>>choice;
+        if(choice =='n')play_tto(COMPUTER);
+        else if(choice =='y')play_tto(HUMAN);
+        else cout<<"INVALID CHOICE\n";
+        cout<<"Do you want to quit(y/n) : ";
+        cin>>cont;
+    }while(cont=='n');
+}
